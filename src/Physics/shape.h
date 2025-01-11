6aeb5cc025588c992c74581c27af578a58ad1d30
EDG429 <125379260+EDG429@users.noreply.github.com>
2025-01-11 18:22:52 +0100

    Started accelerating the rotation with the torque force

--- src/Physics/shape.h
+++ src/Physics/shape.h
@@ -14,7 +14,7 @@ struct Shape {
 	virtual ~Shape() = default;
 	virtual ShapeType GetType() const = 0;
 	virtual Shape* Clone() const = 0;
-	virtual float GetMomentOfInertia() const;
+	virtual float GetMomentOfInertia() const = 0;
 };
 
 struct CircleShape : public Shape {
